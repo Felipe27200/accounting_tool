7f6731286bf5d491670f3d653f092d36335bbf2e
Felipe Zea <48108543+Felipe27200@users.noreply.github.com>
2024-01-10 16:26:50 -0500

    INIT COMMIT

---
+++ src/main/java/com/accounting/accounting_tool/AccountingToolApplication.java
@@ -0,0 +1,13 @@
+package com.accounting.accounting_tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountingToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountingToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/accounting/accounting_tool/AccountingToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accounting.accounting_tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountingToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
